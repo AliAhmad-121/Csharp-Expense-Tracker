7177b750b7f95d2e10b66f94fff03fa15fd3394c
AliAhmad-121 <Aliahmadomar2004@gmail.com>
2025-08-21 15:39:10 +0300

    Add project files.

---
+++ Expense Tracker v2/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Expense_Tracker_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAmount = new System.Windows.Forms.Label();
+            this.txtNote = new System.Windows.Forms.Label();
+            this.cmbCategory = new System.Windows.Forms.Label();
+            this.Date = new System.Windows.Forms.Label();
+            this.type = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.datePicker = new System.Windows.Forms.DateTimePicker();
+            this.rbIncome = new System.Windows.Forms.RadioButton();
+            this.rbExpense = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.AutoSize = true;
+            this.txtAmount.Location = new System.Drawing.Point(98, 12);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(52, 16);
+            this.txtAmount.TabIndex = 0;
+            this.txtAmount.Text = "Amount";
+            this.txtAmount.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtNote
+            // 
+            this.txtNote.AutoSize = true;
+            this.txtNote.Location = new System.Drawing.Point(98, 63);
+            this.txtNote.Name = "txtNote";
+            this.txtNote.Size = new System.Drawing.Size(36, 16);
+            this.txtNote.TabIndex = 1;
+            this.txtNote.Text = "Note";
+            this.txtNote.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // cmbCategory
+            // 
+            this.cmbCategory.AutoSize = true;
+            this.cmbCategory.Location = new System.Drawing.Point(98, 113);
+            this.cmbCategory.Name = "cmbCategory";
+            this.cmbCategory.Size = new System.Drawing.Size(62, 16);
+            this.cmbCategory.TabIndex = 2;
+            this.cmbCategory.Text = "Category";
+            // 
+            // Date
+            // 
+            this.Date.AutoSize = true;
+            this.Date.Location = new System.Drawing.Point(98, 186);
+            this.Date.Name = "Date";
+            this.Date.Size = new System.Drawing.Size(36, 16);
+            this.Date.TabIndex = 3;
+            this.Date.Text = "Date";
+            // 
+            // type
+            // 
+            this.type.AutoSize = true;
+            this.type.Location = new System.Drawing.Point(98, 252);
+            this.type.Name = "type";
+            this.type.Size = new System.Drawing.Size(39, 16);
+            this.type.TabIndex = 4;
+            this.type.Text = "Type";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(174, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(174, 60);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(174, 113);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.Text = "      ";
+            // 
+            // datePicker
+            // 
+            this.datePicker.Location = new System.Drawing.Point(174, 188);
+            this.datePicker.Name = "datePicker";
+            this.datePicker.Size = new System.Drawing.Size(200, 22);
+            this.datePicker.TabIndex = 8;
+            // 
+            // rbIncome
+            // 
+            this.rbIncome.AutoSize = true;
+            this.rbIncome.Location = new System.Drawing.Point(174, 248);
+            this.rbIncome.Name = "rbIncome";
+            this.rbIncome.Size = new System.Drawing.Size(72, 20);
+            this.rbIncome.TabIndex = 9;
+            this.rbIncome.TabStop = true;
+            this.rbIncome.Text = "Income";
+            this.rbIncome.UseVisualStyleBackColor = true;
+            // 
+            // rbExpense
+            // 
+            this.rbExpense.AutoSize = true;
+            this.rbExpense.Location = new System.Drawing.Point(303, 248);
+            this.rbExpense.Name = "rbExpense";
+            this.rbExpense.Size = new System.Drawing.Size(81, 20);
+            this.rbExpense.TabIndex = 10;
+            this.rbExpense.TabStop = true;
+            this.rbExpense.Text = "Expense";
+            this.rbExpense.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(237, 278);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Done";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 307);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(549, 150);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rbExpense);
+            this.Controls.Add(this.rbIncome);
+            this.Controls.Add(this.datePicker);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.type);
+            this.Controls.Add(this.Date);
+            this.Controls.Add(this.cmbCategory);
+            this.Controls.Add(this.txtNote);
+            this.Controls.Add(this.txtAmount);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtAmount;
+        private System.Windows.Forms.Label txtNote;
+        private System.Windows.Forms.Label cmbCategory;
+        private System.Windows.Forms.Label Date;
+        private System.Windows.Forms.Label type;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker datePicker;
+        private System.Windows.Forms.RadioButton rbIncome;
+        private System.Windows.Forms.RadioButton rbExpense;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Expense Tracker v2/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Globalization;   // for number parsing
+
+namespace Expense_Tracker_v2
+{
+    public partial class Form1 : Form
+    {
+        // ---- DB path + connection string (absolute path next to the .exe)
+        private static readonly string DbPath =
+            System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "expenses.db");
+        private static readonly string ConnStr = $"Data Source={DbPath};Version=3;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Ensure DB file exists
+            if (!System.IO.File.Exists(DbPath))
+            {
+                SQLiteConnection.CreateFile(DbPath);
+            }
+
+            // Ensure table exists (handles missing-table crashes)
+            using (var conn = new SQLiteConnection(ConnStr))
+            {
+                conn.Open();
+                string sql = @"CREATE TABLE IF NOT EXISTS Transactions (
+                                  Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                  Amount REAL NOT NULL,
+                                  Category TEXT NOT NULL,
+                                  Note TEXT,
+                                  Date TEXT NOT NULL,
+                                  Type TEXT NOT NULL
+                               )";
+                using (var cmd = new SQLiteCommand(sql, conn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            // Add categories once & lock dropdown to list
+            if (comboBox1.Items.Count == 0)
+            {
+                var cats = new object[] { "Food", "Rent", "Salary", "Transport", "Other" };
+                comboBox1.Items.AddRange(cats);
+            }
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            // Default type selection
+            rbExpense.Checked = true;
+
+            // Load data
+            LoadData();
+
+            // Optional: press Enter to submit
+            this.AcceptButton = button1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Validate amount from textBox1
+            if (!decimal.TryParse(textBox1.Text, System.Globalization.NumberStyles.Number,
+                                  System.Globalization.CultureInfo.InvariantCulture, out var amount))
+            {
+                MessageBox.Show("Please enter a valid numeric amount.", "Invalid Amount",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBox1.Focus();
+                textBox1.SelectAll();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(comboBox1.Text))
+            {
+                MessageBox.Show("Please choose a category.", "Missing Category",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                comboBox1.DroppedDown = true;
+                return;
+            }
+
+            using (var conn = new SQLiteConnection(ConnStr))
+            {
+                conn.Open();
+                string sql = "INSERT INTO Transactions (Amount, Category, Note, Date, Type) " +
+                             "VALUES (@Amount, @Category, @Note, @Date, @Type)";
+                using (var cmd = new SQLiteCommand(sql, conn))
+                {
+                    cmd.Parameters.Add("@Amount", DbType.Double).Value = (double)amount;
+                    cmd.Parameters.AddWithValue("@Category", comboBox1.Text);
+                    cmd.Parameters.AddWithValue("@Note", textBox2.Text); // note from textBox2
+                    cmd.Parameters.AddWithValue("@Date", datePicker.Value.ToString("yyyy-MM-dd"));
+                    cmd.Parameters.AddWithValue("@Type", rbIncome.Checked ? "Income" : "Expense");
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            // Clear inputs
+            textBox1.Clear();
+            textBox2.Clear();
+            comboBox1.SelectedIndex = -1;
+            rbExpense.Checked = true;
+            textBox1.Focus();
+
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            using (var conn = new SQLiteConnection(ConnStr))
+            {
+                conn.Open();
+                string sql = "SELECT * FROM Transactions ORDER BY Date DESC, Id DESC";
+                using (var da = new SQLiteDataAdapter(sql, conn))
+                {
+                    var dt = new DataTable();
+                    da.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                }
+            }
+
+            ConfigureGrid();
+        }
+
+        private void ConfigureGrid()
+        {
+            var gv = dataGridView1;
+
+            gv.AllowUserToAddRows = false;
+            gv.AllowUserToDeleteRows = false;
+            gv.ReadOnly = true;
+            gv.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            gv.MultiSelect = false;
+            gv.RowHeadersVisible = false;
+            gv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            gv.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(245, 245, 245);
+
+            if (gv.Columns.Contains("Id")) gv.Columns["Id"].HeaderText = "#";
+            if (gv.Columns.Contains("Amount"))
+            {
+                gv.Columns["Amount"].HeaderText = "Amount";
+                gv.Columns["Amount"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+                gv.Columns["Amount"].DefaultCellStyle.Format = "N2";
+                gv.Columns["Amount"].FillWeight = 90;
+            }
+            if (gv.Columns.Contains("Category")) gv.Columns["Category"].FillWeight = 120;
+            if (gv.Columns.Contains("Note")) gv.Columns["Note"].FillWeight = 200;
+            if (gv.Columns.Contains("Date"))
+            {
+                gv.Columns["Date"].HeaderText = "Date";
+                gv.Columns["Date"].DefaultCellStyle.Format = "yyyy-MM-dd";
+                gv.Columns["Date"].FillWeight = 110;
+            }
+            if (gv.Columns.Contains("Type")) gv.Columns["Type"].FillWeight = 90;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // not used
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            // not used
+        }
+    }
+}
